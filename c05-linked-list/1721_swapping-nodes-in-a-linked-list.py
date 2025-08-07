22d0fa184a15780548813687da1f185a79866a68
airmaso <itslawmason@gmail.com>
2025-08-06 19:18:58 -0500

    solved 1721_swapping-nodes-in-a-linked-list (optimally)

--- c05-linked-list/1721_swapping-nodes-in-a-linked-list.py
+++ c05-linked-list/1721_swapping-nodes-in-a-linked-list.py
@@ -45,7 +45,42 @@ class Solution:
 
         Assume the list is 1-indexed.
         """
-        pass
+
+        """
+        Optimal solution that uses pointers to get to the swap positions.
+        Uses the fact that once we get to the left swap position, the distance
+        from the head to the left swap position will be the same as the distance
+        from the tail to the right swap position.
+
+        Initially, the rightmost edge of the sliding window will the be left swap
+        position and once the rightmost edge reaches the last node in the linked
+        list (after shifting to the sliding to the right sequentially), the leftmost
+        edge of the sliding window will be the right swap position.
+
+        After that, we can just swap the values to avoid complicated pointer updates and
+        edge case handling.
+
+        Time: O(n)
+        Space: O(1)
+        """
+        left_node = right_node = temp = head
+
+        # shift the left node to its swap position
+        for _ in range(k - 1):
+            left_node = left_node.next
+            temp = temp.next
+
+        # shift until temp is at the last node
+        # (after this loop exits, the right node will be at its swap position)
+        while temp.next:
+            temp = temp.next
+            right_node = right_node.next
+
+        # swap the node values to avoid complicated pointer updates
+        left_node.val, right_node.val = right_node.val, left_node.val
+
+        # just return the head
+        return head
 
 
 if __name__ == "__main__":
